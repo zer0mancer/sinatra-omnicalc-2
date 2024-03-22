1d1ca1dd541a7223965a95bbd5336bf7d8ab664d
Jim Vongsouvanh <95146291+zer0mancer@users.noreply.github.com>
2024-03-22 19:01:47 +0000

    initial commit. changed app.rb with spell checks and corrected file names. Look at logic next and HTML pages render

--- app.rb
+++ app.rb
@@ -5,12 +5,13 @@ get("/") do
   redirect("/add")
 end
 
-get("/ad") do
+get("/add") do
   erb(:add_form)
 end
 
 get("/wizard_add") do
   @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
 
   @result = @first_num + @second_num
   erb(:add_result)
@@ -24,14 +25,21 @@ get("/wizard_subtract") do
   @first_num = params.fetch("first_num").to_f
   @second_num = params.fetch("second_num").to_f
 
+  @result = @first_num - @second_num
+
   erb(:sub_result)
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  @result = @first_num * @second_num
   erb(:mult_result)
 end
 
@@ -40,5 +48,10 @@ get("/divide") do
 end
 
 get("/wizard_divide") do
+
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  @result = @first_num / @second_num
   erb(:div_result)
 end
